6daef96c685227e9062e7797c9d24bbdbe606d28
anishpatel321 <anishpatel213@gmail.com>
2024-03-02 12:54:51 -0700

    at home page new ux

---
+++ client/lib/pages/questions_concerns_page.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class FAQPage extends StatefulWidget {
+  const FAQPage({super.key});
+
+  @override
+  _FAQPageState createState() => _FAQPageState();
+}
+
+class _FAQPageState extends State<FAQPage> {
+  final List<Item> _data = generateItems(5);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('FAQ'),
+      ),
+      body: ListView(
+        children: _data.map<Widget>((Item item) {
+          return ExpansionPanelList(
+            expansionCallback: (int index, bool isExpanded) {
+              setState(() {
+                item.isExpanded = !isExpanded;
+              });
+            },
+            children: [
+              ExpansionPanel(
+                headerBuilder: (BuildContext context, bool isExpanded) {
+                  return ListTile(
+                    title: Text(item.headerValue),
+                  );
+                },
+                body: ListTile(
+                  title: Text(item.expandedValue),
+                ),
+                isExpanded: item.isExpanded,
+              ),
+            ],
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
+
+class Item {
+  Item({
+    required this.expandedValue,
+    required this.headerValue,
+    this.isExpanded = false,
+  });
+
+  String expandedValue;
+  String headerValue;
+  bool isExpanded;
+}
+
+List<Item> generateItems(int numberOfItems) {
+  return List<Item>.generate(numberOfItems, (int index) {
+    return Item(
+      headerValue: 'Question $index',
+      expandedValue: 'Answer $index',
+    );
+  });
+}
